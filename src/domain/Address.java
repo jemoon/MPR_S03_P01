d7b5fc7d65d2f851a7923a99d76649726a4232f4
Marcin Jędrzejewski <s10400@pjwstk.edu.pl>
2014-10-01 21:37:34 +0200

    - Address class (forgot that Eclipse doesnt have autosave as IteliJ)

--- src/domain/Address.java
+++ src/domain/Address.java
@@ -1,5 +1,58 @@
 package domain;
 
 public class Address {
+	private String country;
+	private String city;
+	private String postalCode;
+	private String street;
+	private String houseNumber;
+	private Employee employee;
 
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+
+	public void setHouseNumber(String houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
 }
